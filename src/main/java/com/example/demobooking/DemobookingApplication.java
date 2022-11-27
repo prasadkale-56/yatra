4ccf7d9cbcde5656e771fbacb3afcd525c1dfdfe
Prasad Kale <Prasad.Kale@splus.local>
2022-11-27 09:56:44 +0530

    codepush

---
+++ src/main/java/com/example/demobooking/DemobookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobookingApplication.class, args);
+	}
+
+}
